0001489dc3182a6f90ad43cad05d062fad3f7ea3
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-08 14:59:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const posts = [
+  {
+    title: "How to find JavaScript developer job?",
+    postId: 3421,
+    comments: 25
+  },
+  {
+    title: "Is it hard to learn ES6?",
+    postId: 5216,
+    comments: 3
+  },
+  {
+    title: "Should I learn React or Angular?",
+    postId: 8135,
+    comments: 12
+  }
+];
+
+const minimalComentsQty = 10;
+
+/* Create a function "popularPostsIds" with two parameters "posts" and "minimalComentsQty".
+
+This function "popularPostsIds" should return an array of postIds of posts that have quantity of "comments" at least equal to "minimalComentsQty"
+*/
+
+const popularPostsIds = arrayOfPostId => 
+arrayOfPostId.reduce(
+    (idArray, posts) => {
+        if(posts.comments >= minimalComentsQty)
+        idArray.push(posts.postId);
+        return idArray;
+    },[]);
+
+
+
+console.log(popularPostsIds(posts, minimalComentsQty)); // [3421, 8135]
